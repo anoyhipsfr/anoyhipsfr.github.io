2a57208f0180c27d087d96a5c7bdb96f5f09964d
anoyhipsfr <125494841+anoyhipsfr@users.noreply.github.com>
2025-05-29 10:09:02 -0700

    update?

--- doom/doomgeneric.js
+++ doom/doomgeneric.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpg5f6n_2s.js
+// include: /tmp/tmpzawflnl8.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -207,25 +207,25 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
     }
 
     }
-    loadPackage({"files": [{"filename": "/doom.wad", "start": 0, "end": 11159840}], "remote_package_size": 11159840});
+    loadPackage({"files": [{"filename": "/doom1.wad", "start": 0, "end": 11159840}], "remote_package_size": 11159840});
 
   })();
 
-// end include: /tmp/tmpg5f6n_2s.js
-// include: /tmp/tmpywzz0puv.js
+// end include: /tmp/tmpzawflnl8.js
+// include: /tmp/tmp_lpc85sc.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpywzz0puv.js
-// include: /tmp/tmpbpmyvna8.js
+  // end include: /tmp/tmp_lpc85sc.js
+// include: /tmp/tmpmq0naz0s.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpbpmyvna8.js
+  // end include: /tmp/tmpmq0naz0s.js
 
 
 var arguments_ = [];
